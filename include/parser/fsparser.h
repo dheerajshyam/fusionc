e49d6e16a09c7c577ba382741c2d6a914397394a
dheerajshyam <dheerajshyam2020@gmail.com>
2024-02-06 20:54:24 +0530

    Started recursive descent parsing.

--- include/parser/fsparser.h
+++ include/parser/fsparser.h
@@ -8,10 +8,67 @@
 namespace fusion_parser {
 
     // CST Related Code.
-    class Node {
-    public:
-        virtual ~Node() = default;
+    enum CstNodeType {
+        Expr,
+        TernaryExpr,
+        BinaryExpr,
+        OrOp,
+        IsNotOp,
+        IsntOp,
+        AndOp,
+        GtOp,
+        AddOp,
+        MulOp,
+        ExponentOp,
+        UnaryExpr,
+        Object,
+        FuncCall,
+        Params,
+        Param,
+        TypeObject,
+        String
     };
+
+    typedef class CstNode {
+    public:
+        CstNodeType type;
+        virtual ~CstNode() = default;
+    } fs_cst;
+
+
+    typedef class ExprCstNode : public CstNode {
+    protected:
+        fs_cst* _ternExpr = nullptr;
+    public:
+        fs_cst* ternaryExpr();
+        void setTernExpr(fs_cst *);
+    } fs_exprCst;
+
+    typedef class TernaryExprCstNode : public CstNode {
+    protected:
+        fs_cst* _binExpr;
+        fs_cst* ternExpr[2];
+    public:
+        fs_cst* binaryExpr();
+        fs_cst* ternaryExpr(int); // Returns particular ternaryExpression.
+
+        void setBinExpr(fs_cst *);
+        void setTernExpr(fs_cst *, int);
+
+    } fs_ternExprCst;
+
+    typedef class BinaryExprCstNode : public CstNode {
+    protected:
+        fs_cst* _opOp;
+    public:
+        fs_cst* orOp();
+        void setOrOp(fs_cst *);
+    } fs_binExprCst;
+
+    typedef class OrOpCstNode : public CstNode {
+    public:
+
+    } fs_orOpCst;
 
     // Context related code.
     class Context {
@@ -23,14 +80,29 @@ namespace fusion_parser {
     class Parser {
     protected:
         fusion_lexer::Lexer* lexer;
+
+        vector<fusion_lexer::fs_token*>::iterator it;
+        vector<fusion_lexer::fs_token*>::iterator end;
     public:
         explicit Parser(fusion_lexer::Lexer *);
 
-        void* binaryExpr();
-        void* BinaryExpr(); // ternaryExpr form 2.
-        void* TernaryExpr(); // ternaryExpr form 1.
-        void* expr();
-        void* start();
+        fs_cst* param();
+        fs_cst* params();
+        fs_cst* func_call();
+        fs_cst* object();
+        fs_cst* unaryExpr();
+        fs_cst* exponent_op();
+        fs_cst* mul_op();
+        fs_cst* add_op();
+        fs_cst* gt_op();
+        fs_cst* and_op();
+        fs_cst* isnt_op();
+        fs_cst* is_not_op();
+        fs_cst* or_op();
+        fs_cst* binaryExpr();
+        fs_cst* ternaryExpr();
+        fs_cst* expr();
+        fs_cst* start();
         ~Parser();
     };
 }
--- src/parser/fsparser.cpp
+++ src/parser/fsparser.cpp
@@ -1,13 +1,157 @@
 #include "fsparser.h"
 
 namespace fusion_parser {
-    Parser::Parser(fusion_lexer::Lexer *lexer) {
-        this->lexer = lexer;
+
+    // Cst node related code.
+    void ExprCstNode::setTernExpr(
+        fusion_parser::fs_cst *node) {
+        this->_ternExpr = node;
     }
 
-    void* Parser::start() {
-        for(auto const& token : lexer->get_tokens())
-            cout << token->type << endl;
+    fs_cst *ExprCstNode::ternaryExpr() {
+        return this->_ternExpr;
+    }
+
+    void TernaryExprCstNode::setTernExpr(
+        fusion_parser::fs_cst *node, int index) {
+        this->ternExpr[index] = node;
+    }
+
+    fs_cst *TernaryExprCstNode::ternaryExpr(int index) {
+        if(index > 1)
+            return nullptr;
+        return this->ternExpr[index];
+    }
+
+    fs_cst *TernaryExprCstNode::binaryExpr() {
+        return this->_binExpr;
+    }
+
+    void TernaryExprCstNode::setBinExpr(
+        fusion_parser::fs_cst *node) {
+        this->_binExpr = node;
+    }
+
+    // Parser related code.
+    Parser::Parser(fusion_lexer::Lexer *lexer) {
+        this->lexer = lexer;
+        this->it = this->lexer->get_tokens().begin();
+        this->end = this->lexer->get_tokens().end();
+    }
+
+    fs_cst* Parser::param() {
+
+    }
+
+    fs_cst* Parser::params() {
+
+    }
+
+    fs_cst* Parser::func_call() {
+    }
+
+    fs_cst* Parser::object() {
+
+    }
+
+    fs_cst* Parser::unaryExpr() {
+
+    }
+
+    fs_cst* Parser::exponent_op() {
+
+    }
+
+    fs_cst* Parser::mul_op() {
+
+    }
+
+    fs_cst* Parser::add_op() {
+
+    }
+
+    fs_cst* Parser::gt_op() {
+
+    }
+
+    fs_cst* Parser::and_op() {
+
+    }
+
+    fs_cst *Parser::isnt_op() {
+
+    }
+
+    fs_cst* Parser::is_not_op() {
+
+    }
+
+    fs_cst* Parser::or_op() {
+
+    }
+
+    fs_cst* Parser::binaryExpr() {
+        auto* node = new BinaryExprCstNode();
+        node->type = CstNodeType::BinaryExpr;
+        node->setOrOp(or_op());
+
+        return node;
+    }
+
+    fs_cst* Parser::ternaryExpr() {
+
+        auto* node = new fs_ternExprCst();
+        node->type = CstNodeType::TernaryExpr;
+
+        fusion_lexer::fs_token* nextToken;
+        std::string nextTokType, nextTokVal;
+
+        it++;
+
+        node->setBinExpr(binaryExpr());
+
+        nextToken = *it;
+        nextTokType = nextToken->type;
+        nextTokVal = any_cast<std::string>(
+        FS_VarGet(nextToken->value));
+
+        if(nextTokType == "KEYWORD"
+            && nextTokVal == "if") {
+            it++;
+
+            node->setTernExpr(ternaryExpr(), 0);
+
+            nextToken = *it;
+            nextTokType = nextToken->type;
+            nextTokVal = any_cast<std::string>(
+            FS_VarGet(nextToken->value));
+
+            if(nextTokType == "KEYWORD"
+                && nextTokVal == "else")
+                node->setTernExpr(ternaryExpr(), 1);
+            else {
+                delete this->lexer;
+                FsIO_Print(stderr, FsVal_ToFsVar(
+               any(string("syntax error: expected else after if in"
+                  "ternary expression in line " + std::to_string(nextToken->lineno) + "."))
+                ));
+                exit(0);
+            }
+        }
+
+        return node;
+    }
+
+    fs_cst* Parser::expr() {
+        auto* node = new fs_exprCst();
+        node->type = CstNodeType::Expr;
+        node->setTernExpr(ternaryExpr());
+
+        return node;
+    }
+
+    fs_cst* Parser::start() {
+        return expr();
     }
 
     Parser::~Parser() {
